719f54cbb6c261da4a77448c8097b7e2e229ef47
littishathankam <littishathankam@HP>
2018-01-19 10:45:04 +0530

    initial commit

---
+++ New/src/com/ne/Test.java
@@ -0,0 +1,12 @@
+package com.ne;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Test from 1");
+System.out.println("Test from 2");
+System.out.println("Test from 3");
+	}
+
+}
